5d4e7839cdc26a385e00085f5bde0bdf76629d84
Dennis Padiernos <depadiernos@gmail.com>
2019-12-10 20:03:37 -0600

    set up express server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code away!
+const server = require("./server")
+const userRouter = require("./users/userRouter")
+const postRouter = require("./posts/postRouter")
+
+const port = 3000;
+const host = "127.0.0.1";
+
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}`);
+  });
+
+server.use("/api/users", userRouter)
+server.use("/api/users/:id/posts", postRouter)
